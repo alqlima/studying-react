3813b7bf53575fc508529319b01121d9e786cdc6
André Queiroz <alqlima@gmail.com>
2020-12-06 10:06:53 -0300

    Create Sobrenome.js

---
+++ app2/src/componentes/Sobrenome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Sobrenome extends React.Component {
+    render() {
+        return (
+            <p>Surname: Queiroz</p>
+        )
+    }
+}
+export default Sobrenome;

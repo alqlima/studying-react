e98dd425d60a38cf5a3da2d2abdbc43cae340959
André Queiroz <alqlima@gmail.com>
2020-12-27 11:27:11 -0300

    Create Child.js

---
+++ app2/src/componentes/Child.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Child extends Component {
+    // Alterar o nome do parent component
+    Alterar = () => {
+        this.props.funcaoAlterar('Este é o novo nome');
+    }
+    render() {
+        return (
+            <div>
+                <h3>Child</h3>
+                <button onClick={this.Alterar}>Alterar</button>
+            </div>
+        )
+    }
+}
+
+export default Child;
